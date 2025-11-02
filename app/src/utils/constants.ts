e4cc0e169d2ee00e3f006fbf77094fbba85b861b
Alexander Terp <alexander.terp@gmail.com>
2025-11-02 19:15:30 +1100

    attempt to fix deployed website 2

--- app/src/utils/constants.ts
+++ app/src/utils/constants.ts
@@ -1,5 +1,5 @@
-export const SERIES_URL = '/data/';
-export const TITLE_ID_URL = '/data/titleId.json';
+export const SERIES_URL = `${import.meta.env.BASE_URL}data/`;
+export const TITLE_ID_URL = `${import.meta.env.BASE_URL}data/titleId.json`;
 export const IMDB_URL = 'https://www.imdb.com/title/';
 
 // Color thresholds
